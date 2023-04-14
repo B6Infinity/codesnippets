881edbaa6ed39ec52990028d9a3e2a053b92a37e
Broteen Das <broteendas.official@gmail.com>
2023-04-14 19:32:39 +0530

    Update subtle_defs.dart

--- Flutter/utils/subtle_defs.dart
+++ Flutter/utils/subtle_defs.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: avoid_print
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 Widget h3txt(String txt,
     {int multiplier = 1, Color? color, double padding = 8.0}) {
@@ -36,4 +39,30 @@ Widget h3heading(String txt,
       ),
     ],
   );
+}
+
+Future copyTextToClipboard(String txt) async {
+  await Clipboard.setData(ClipboardData(text: txt));
+}
+
+void showSnackBarMSG(BuildContext context, String msg) {
+  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+    content: Text(msg),
+  ));
+}
+
+void printG(String str) {
+  print('\u001b[32m$str\u001b[0m');
+}
+
+void printB(String str) {
+  print('\u001b[34m$str\u001b[0m');
+}
+
+void printM(String str) {
+  print('\u001b[35m$str\u001b[0m');
+}
+
+void printC(String str) {
+  print('\u001b[36m$str\u001b[0m');
 }
