795c53ee2a82efa3fd589b3edb8772c23ad95c81
Broteen Das <broteendas.official@gmail.com>
2023-05-16 17:44:30 +0530

    Create subtle_defs.py

---
+++ Python/subtle_defs.py
@@ -0,0 +1,11 @@
+def printG(s):
+  print(f'\u001b[32m{s}\u001b[0m')
+
+def printB(s):
+  print(f'\u001b[34m{s}\u001b[0m')
+
+def printM(s):
+  print(f'\u001b[35m{s}\u001b[0m')
+
+def printC(s):
+  print(f'\u001b[36m{s}\u001b[0m')
