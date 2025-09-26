27252fd82b4f3040654b20a94fae35e993581800
Broteen Das <broteendas.official@gmail.com>
2025-09-26 11:43:51 +0530

    Create LPTIM1_PWM.c

---
+++ STMCubeIDE/STM32L031G6U/LPTIM1_PWM.c
@@ -0,0 +1,26 @@
+/*
+-----------PLEASE REMOVE THIS COMMENT CHUNK BEFORE PLACING IT IN THE CUBEIDE-----------
+In the IOC file:
+1. Go to Timers > LPTIM1:
+2. Mode: Count internal clock events
+  2. a. Enable Waveform Generation
+3. Configuration: Configure the "Clock Prescaler" and "Output" as necessary.
+
+*/
+
+... 
+
+int main(void)
+{
+  uint32_t period = 33; // ARR
+  uint32_t pulse  = 24;   // CMP (0.8 * 30000 duty)
+
+  HAL_LPTIM_PWM_Start(&hlptim1, period, pulse);
+
+  ...
+
+  while (1)
+  {
+    
+  }
+}
